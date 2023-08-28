c6cfcbc715b6a3eda0d868fe15b9a8cd50421cd0
Kemboi Lewis <kemboilewis@gmal.com>
2023-08-28 06:59:01 +0300

    modified simple calculator

--- simple_calculator.py
+++ simple_calculator.py
@@ -5,17 +5,18 @@ try:
     number_1, number_2 = float(number_1), float(number_2)
     if operation == 'm':
         result = number_1*number_2
-        print("{} * {} = {}".format(number_1,number_2,result))
+        print("{} * {} = {}".format(number_1, number_2, result))
     elif operation == 'd':
         result = number_1/number_2
-        print("{} / {} = {}".format(number_1,number_2,result))
+        print("{} / {} = {}".format(number_1, number_2, result))
     elif operation == 's':
         result = number_1 - number_2
-        print("{} - {} = {}".format(number_1,number_2,result))
+        print("{} - {} = {}".format(number_1, number_2, result))
     elif operation == 's':
         result = number_1 + number_2
-        print("{} + {} = {}".format(number_1,number_2,result))
+        print("{} + {} = {}".format(number_1, number_2, result))
     else:
         print("You entered invalid operation")
+
 except:
     print("Invalid numbers")
